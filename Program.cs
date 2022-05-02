00eb348cb8be08fc2c7078a9fe9b22ee328aca7e
Dominik Pflegerl <github.me@neus.xyz>
2022-05-02 08:22:07 +0200

    main functionality established. I am bad at commiting.

--- Program.cs
+++ Program.cs
@@ -1,6 +1,64 @@
-﻿// See https://aka.ms/new-console-template for more information
-class TruckTransmissionGenerator {
+﻿using System.Data;
+using Newtonsoft.Json;
+public class TruckTransmissionGenerator {
     static void Main(string[] args) {
-        Console.WriteLine("Hello, World!");
+        string jsonFile = @"C:\Users\Dominik\OneDrive - Dominik Pflegerl\Projects\Modding\American Truck Simulator\Allison Transmission\data\data.json";
+        string templateFolder = @"C:\Users\Dominik\OneDrive - Dominik Pflegerl\Projects\Modding\American Truck Simulator\Allison Transmission\data\transmission_templates";
+        string outputFolder = @"C:\Users\Dominik\OneDrive - Dominik Pflegerl\Projects\Modding\American Truck Simulator\Allison Transmission\output";
+        string[] transmissionsFiles = Directory.GetFiles(templateFolder, "*.sii");
+
+        StreamReader r = new StreamReader(jsonFile);
+        string jsonString = r.ReadToEnd();
+        dynamic jsonData = JsonConvert.DeserializeObject(jsonString);
+        
+        foreach(var truck in jsonData?.trucks) {
+            foreach(var transmission in jsonData?.transmissions) {
+                foreach(var differential_ratio in jsonData?.differential_ratios) {
+
+                    Console.WriteLine(transmission.name + " " + differential_ratio);
+
+                    string siiFile = File.ReadAllText(templateFolder + @"\" + transmission.name + ".sii");
+                    siiFile = siiFile.Replace("@name",transmission.name.ToString());
+                    siiFile = siiFile.Replace("@differential_ratio_name",differential_ratio.ToString().Replace(".",""));
+                    siiFile = siiFile.Replace("@truck",truck.ToString());
+                    siiFile = siiFile.Replace("@displayName",$"\"{transmission.displayName.ToString()}\"");
+                    siiFile = siiFile.Replace("@price",transmission.price.ToString());
+                    siiFile = siiFile.Replace("@unlock",transmission.unlock.ToString());
+                    siiFile = siiFile.Replace("@differential_ratio",differential_ratio.ToString());
+
+                    //Console.Write(siiFile);
+
+
+                    string outputFolderFull = outputFolder + @"\def\vehicle\truck\" + truck.ToString() + @"\transmission\";
+                    string outputFile = outputFolderFull + transmission.name.ToString() + "." + differential_ratio.ToString().Replace(".","") + ".sii";
+                    Console.WriteLine(outputFolderFull);
+
+                    if(!Directory.Exists(outputFolderFull)) { Directory.CreateDirectory(outputFolderFull); }
+                    File.WriteAllText(outputFile,siiFile);
+
+                    Console.WriteLine("\n---------------------\n");
+                }
+            }
+        }
+        
+
+
+        //Array.ForEach(transmissionsFiles,Console.WriteLine);
+        //
+        //string siiFile = File.ReadAllText(transmissionsFiles[0]);
+        //siiFile = siiFile.Replace("@name",jsonData.transmissions[0].name.ToString());
+        //siiFile = siiFile.Replace("$differential_ratio_name",jsonData.differential_ratios[0].ToString().Replace(".",""));
+        //siiFile = siiFile.Replace("$truck",jsonData.trucks[8].ToString());
+        //siiFile = siiFile.Replace("$displayName",$"\"{jsonData.transmissions[0].displayName.ToString()}\"");
+        //siiFile = siiFile.Replace("@price",jsonData.transmissions[0].price.ToString());
+        //siiFile = siiFile.Replace("@unlock",jsonData.transmissions[0].unlock.ToString());
+        //siiFile = siiFile.Replace("$differential_ratio",jsonData.differential_ratios[0].ToString());
+        //
+        //Console.Write(siiFile);
+        //
+        //
+        //
+        //File.WriteAllText(templateFolder + @"\test.txt",siiFile);
+
     }
 }
