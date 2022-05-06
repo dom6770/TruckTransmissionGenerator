488bdab63ea4ce5fabb95c478472c4276c1476f5
Dominik Pflegerl <github.me@neus.xyz>
2022-05-06 20:04:13 +0200

    Added json variables for folders, try-catch and argument (json file) validator

--- Program.cs
+++ Program.cs
@@ -2,63 +2,70 @@
 using Newtonsoft.Json;
 public class TruckTransmissionGenerator {
     static void Main(string[] args) {
-        string jsonFile = @"C:\Users\Dominik\OneDrive - Dominik Pflegerl\Projects\Modding\American Truck Simulator\Allison Transmission\data\data.json";
-        string templateFolder = @"C:\Users\Dominik\OneDrive - Dominik Pflegerl\Projects\Modding\American Truck Simulator\Allison Transmission\data\transmission_templates";
-        string outputFolder = @"C:\Users\Dominik\OneDrive - Dominik Pflegerl\Projects\Modding\American Truck Simulator\Allison Transmission\output";
-        string[] transmissionsFiles = Directory.GetFiles(templateFolder, "*.sii");
+        string jsonFile;
 
+        // check if jsonFile has been passed as argument, if not ask for input
+        if(args.Length == 0) {
+            Console.WriteLine("Please specify json location");
+            Console.Write("Input: ");
+            jsonFile = Console.ReadLine();
+        } else {
+            Console.Write("args[0]: ");
+            Console.WriteLine(args[0]);
+            jsonFile = args[0];
+        }
+
+        // read json file
         StreamReader r = new StreamReader(jsonFile);
         string jsonString = r.ReadToEnd();
-        dynamic jsonData = JsonConvert.DeserializeObject(jsonString);
-        
-        foreach(var truck in jsonData?.trucks) {
-            foreach(var transmission in jsonData?.transmissions) {
-                foreach(var differential_ratio in jsonData?.differential_ratios) {
+        dynamic jsonData = JsonConvert.DeserializeObject(jsonString); 
 
-                    Console.WriteLine(transmission.name + " " + differential_ratio);
+        // set necessary variables
+        string templateFolder;
+        string outputFolder;
+        string[] transmissionsFiles;
 
-                    string siiFile = File.ReadAllText(templateFolder + @"\" + transmission.name + ".sii");
-                    siiFile = siiFile.Replace("@name",transmission.name.ToString());
-                    siiFile = siiFile.Replace("@differential_ratio_name",differential_ratio.ToString().Replace(".",""));
-                    siiFile = siiFile.Replace("@truck",truck.ToString());
-                    siiFile = siiFile.Replace("@displayName",$"\"{transmission.displayName.ToString()}\"");
-                    siiFile = siiFile.Replace("@price",transmission.price.ToString());
-                    siiFile = siiFile.Replace("@unlock",transmission.unlock.ToString());
-                    siiFile = siiFile.Replace("@differential_ratio",differential_ratio.ToString());
+        try {
+            templateFolder = jsonData?.templateFolder;
+            transmissionsFiles = Directory.GetFiles(templateFolder,"*.sii");
 
-                    //Console.Write(siiFile);
+            outputFolder = jsonData?.outputFolder;
 
-
-                    string outputFolderFull = outputFolder + @"\def\vehicle\truck\" + truck.ToString() + @"\transmission\";
-                    string outputFile = outputFolderFull + transmission.name.ToString() + "." + differential_ratio.ToString().Replace(".","") + ".sii";
-                    Console.WriteLine(outputFolderFull);
-
-                    if(!Directory.Exists(outputFolderFull)) { Directory.CreateDirectory(outputFolderFull); }
-                    File.WriteAllText(outputFile,siiFile);
-
-                    Console.WriteLine("\n---------------------\n");
+            foreach(var truck in jsonData?.trucks) {
+                foreach(var transmission in jsonData?.transmissions) {
+                    foreach(var differential_ratio in jsonData?.differential_ratios) {
+            
+                        Console.WriteLine(transmission.name + " " + differential_ratio);
+            
+                        string siiFile = File.ReadAllText(templateFolder + @"\" + transmission.name + ".sii");
+                        siiFile = siiFile.Replace("@name",transmission.name.ToString());
+                        siiFile = siiFile.Replace("@differential_ratio_name",differential_ratio.ToString().Replace(".",""));
+                        siiFile = siiFile.Replace("@truck",truck.ToString());
+                        siiFile = siiFile.Replace("@displayName",$"\"{transmission.displayName.ToString()}\"");
+                        siiFile = siiFile.Replace("@price",transmission.price.ToString());
+                        siiFile = siiFile.Replace("@unlock",transmission.unlock.ToString());
+                        siiFile = siiFile.Replace("@differential_ratio",differential_ratio.ToString());
+            
+                        //Console.Write(siiFile);
+            
+            
+                        string outputFolderFull = outputFolder + @"\def\vehicle\truck\" + truck.ToString() + @"\transmission\";
+                        string outputFile = outputFolderFull + transmission.name.ToString() + "." + differential_ratio.ToString().Replace(".","") + ".sii";
+                        Console.WriteLine(outputFolderFull);
+            
+                        if(!Directory.Exists(outputFolderFull)) { Directory.CreateDirectory(outputFolderFull); }
+                        File.WriteAllText(outputFile,siiFile);
+            
+                        Console.WriteLine("\n---------------------\n");
+                    }
                 }
             }
+
         }
-        
-
-
-        //Array.ForEach(transmissionsFiles,Console.WriteLine);
-        //
-        //string siiFile = File.ReadAllText(transmissionsFiles[0]);
-        //siiFile = siiFile.Replace("@name",jsonData.transmissions[0].name.ToString());
-        //siiFile = siiFile.Replace("$differential_ratio_name",jsonData.differential_ratios[0].ToString().Replace(".",""));
-        //siiFile = siiFile.Replace("$truck",jsonData.trucks[8].ToString());
-        //siiFile = siiFile.Replace("$displayName",$"\"{jsonData.transmissions[0].displayName.ToString()}\"");
-        //siiFile = siiFile.Replace("@price",jsonData.transmissions[0].price.ToString());
-        //siiFile = siiFile.Replace("@unlock",jsonData.transmissions[0].unlock.ToString());
-        //siiFile = siiFile.Replace("$differential_ratio",jsonData.differential_ratios[0].ToString());
-        //
-        //Console.Write(siiFile);
-        //
-        //
-        //
-        //File.WriteAllText(templateFolder + @"\test.txt",siiFile);
-
+        catch(Exception e) {
+            Console.WriteLine("FATAL - Please set a valid path for 'templateFolder' and 'outputFolder' in your .json!");
+            Console.WriteLine(e.Message);
+            Console.WriteLine(e.StackTrace);
+        }
     }
 }
